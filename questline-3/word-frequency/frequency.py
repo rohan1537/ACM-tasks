f0335fbeed6ca6af388956f157c9cabb47ddc779
rohan1537 <rohansnair5@gmail.com>
2025-09-07 20:09:12 +0530

    Update frequency.py

--- questline-3/word-frequency/frequency.py
+++ questline-3/word-frequency/frequency.py
@@ -1,1 +1,12 @@
+text = input('Enter a text:')
+words = text.split()
+freq = {}
 
+for word in words:
+    if word in freq:
+        freq[word] += 1
+    else:
+        freq[word] = 1
+
+for word in freq:
+    print(word, "→", freq[word])
