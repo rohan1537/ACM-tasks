771301d426bf4e69c36ff4baf4cceb3e68ff6270
rohan1537 <rohansnair5@gmail.com>
2025-09-09 21:57:28 +0530

    Update palindrome.py

--- questline-3/palindrome/palindrome.py
+++ questline-3/palindrome/palindrome.py
@@ -1,1 +1,12 @@
+s = input("Enter a string: ")
+result = ""
 
+for i in range(len(s)):
+    for a, b in [(i, i), (i, i+1)]:
+        while a >= 0 and b < len(s) and s[a] == s[b]:
+            a -= 1
+            b += 1
+        if b - a - 1 > len(result):
+            result = s[a+1:b]
+
+print("Longest Palindromic Substring:", result)
